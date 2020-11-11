c79141a43df3e4055528a0c685b420cde64c2ec0
Shyam Kumar <shyam.sk.pra@gmail.com>
2020-11-11 14:45:03 +0530

    -- Implemented logout functionality

--- src/Components/Home/index.jsx
+++ src/Components/Home/index.jsx
@@ -1,5 +1,7 @@
+import { Button } from "@material-ui/core";
 import React from "react";
 import { useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
 import CustomTable from "../Shared/CustomTable";
 import "./home.style.css";
 
@@ -11,33 +13,47 @@ import "./home.style.css";
 */
 
 const Home = () => {
-  const data = useSelector(state => state.appCommon.data)
-  const userData = JSON.parse(localStorage.user_details)
+  const history = useHistory();
+  const data = useSelector((state) => state.appCommon.data);
+  const userData = JSON.parse(localStorage.user_details);
+
+  const handleOnLogout = () => {
+    history.push('/login');
+    localStorage.clear();
+  }
 
   return (
-      <div className="homeMainContainer">
-        <div className="homeContainer1">
-          <br />
-          <section>
-            <span>{`Hi `}</span>
-            <span>
-              <b>{userData.name}</b>
-            </span>
-          </section>
-        </div>
-        <div className="tableContainer"> 
-          <CustomTable
-            data={data}
-            columns={[
-              { label: 'Name', dataField: 'name'},
-              { label: 'Age', dataField: 'age'},
-              { label: 'Gender', dataField: 'gender'},
-              { label: 'Email', dataField: 'email'},
-              { label: 'Phone Number', dataField: 'phoneNo'},
-            ]}
-          />
-        </div>
+    <div className="homeMainContainer">
+      <Button
+        variant="contained"
+        color="secondary"
+        onClick={handleOnLogout}
+      >
+        Log Out
+      </Button>
+      <div className="homeContainer1">
+        <br />
+        <section>
+          <span>{`Hi `}</span>
+          <span>
+            <b>{userData.name}</b>
+          </span>
+        </section>
       </div>
+      <div className="tableContainer">
+        <div className="tableHeader">Employee List</div>
+        <CustomTable
+          data={data}
+          columns={[
+            { label: "Name", dataField: "name" },
+            { label: "Age", dataField: "age" },
+            { label: "Gender", dataField: "gender" },
+            { label: "Email", dataField: "email" },
+            { label: "Phone Number", dataField: "phoneNo" },
+          ]}
+        />
+      </div>
+    </div>
   );
 };
 
